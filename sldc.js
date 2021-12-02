acb9691445846b2fbbdcf06bac01d60e8314dcbf
EliseH42 <elise.hofer42@gmail.com>
2021-12-02 16:22:34 -0700

    variables assignment completed

---
+++ sldc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Analysis of Requirements
+// Planning
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "Identify the problem. What do we need to fix or solve? What does it need to do?"
+const design = "Figure out how it's going to accomplish its task. What will it be written in, what systems does it work with, high level how will it work"
+const implementation = "Make it! Write the code."
+const testingAndIntegration = "Make sure what you've made works like it's supposed to. Test regular use, edge cases, evereything you can think of. Break it! Figure out how it breaks and fix every problem you can identify before it goes out to the customer."
+const maintain = "identify and execute steps needed to ensure the program continues to function as time goes on."
