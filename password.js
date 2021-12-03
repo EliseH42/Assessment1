66bf2b5c2e8bd95ffd8d8ea0d65c3124ec1fcc03
EliseH42 <elise.hofer42@gmail.com>
2021-12-02 19:29:25 -0700

    ascii working

--- password.js
+++ password.js
@@ -1,33 +1,34 @@
-// const readline = require('readline').createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-//   });
+const { SSL_OP_NO_QUERY_MTU } = require('constants');
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 
 
 console.log("Welcome to the password validator tool!")
 console.log(""+
-""+
-"                                                    .___"+
-"___________    ______ ________  _  _____________  __| _/"+
-"\____ \__  \  /  ___//  ___/\ \/ \/ /  _ \_  __ \/ __ | "+
-"|  |_> > __ \_\___ \ \___ \  \     (  <_> )  | \/ /_/ | "+
-"|   __(____  /____  >____  >  \/\_/ \____/|__|  \____ | "+
-"|__|       \/     \/     \/                          \/ "+
-""+
-"")
+"\n"+
+"                                                    .___\n"+
+"___________    ______ ________  _  _____________  __| _/\n"+
+"|____ \\__  \\  /  ___//  ___/\\ \\/ \\/ /  _ \\_  __ \\/ __ | \n"+
+"|  |_> > __ \\_\\___ \\ \\___ \\  \\     (  <_> )  | \\/ /_/ | \n"+
+"|   __(____  /____  >____  >  \\/\\_/ \\____/|__|  \\____ | \n"+
+"|__|       \\/     \\/     \\/                          \\/ \n"+
+"\n"+
+"\n")
 
 
-// readline.question("Please enter a password to validate:"), password => {
-    
-//     readline.close();
-// }
+reader.question("Please enter a password to validate:"), function(password){
 
-var password = "1pppa"
-var regex = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g;
+    var regex = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g;
 
-if (password.length > 10 && hasnumber(password)&& regex.test(password === false)){
-    console.log("This is a good password.")
-}else{
-    console.log("Please pick a better password.")
+    if (password.length > 10 && hasnumber(password)&& regex.test(password === false)){
+        console.log("This is a good password.")
+    }else{
+        console.log("Please pick a better password.")
+    }
 }
+reader.close()
